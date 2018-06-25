bbb26b2c54c01a718ff056e46e8c9a0b3ad2cc70
Yehiyam Livneh <yehiyam@gmail.com>
2018-06-25 12:59:33 +0300

    throw execption if config file not found. add lint and pre-commit. add unittests

--- tests/tests.js
+++ tests/tests.js
@@ -15,6 +15,26 @@ describe('config-it', function () {
             done();
 
         });
+        it('should throw if env is not found in logger', function (done) {
+            configIt._env='notExist';
+            const configTest = ()=> configIt.load({ useBase: true, configFolder: 'tests' });
+            expect(configTest).to.throw('Unable to load config file for environment notExist at folder /home/yehil/dev/hkube-common/config.hkube/tests/config/logger. The missing file is tests/config/logger/config.notExist.js')
+            done();
+
+        });
+
+        it('should throw if env is not found in main', function (done) {
+            configIt._env='onlyLogger';
+            const configTest = ()=> configIt.load({ useBase: true, configFolder: 'tests' });
+            expect(configTest).to.throw('Unable to load config file for environment onlyLogger at folder /home/yehil/dev/hkube-common/config.hkube/tests/config/main. The missing file is tests/config/main/config.onlyLogger.js')
+            done();
+
+        });
+
+        it('should get env',()=>{
+            configIt._env='myEnv';
+            expect(configIt.env()).to.eql('myEnv')
+        })
     });
 });
 
